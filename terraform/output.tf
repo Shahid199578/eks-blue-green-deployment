4c44e310782c7bd1edd0f8c893b1600c23b262d6
Mohd Shahid <shahid1995578@gmail.com>
2025-04-19 15:14:10 +0530

    commit

--- terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "eks_cluster_name" {
-  description = "Name of EKS Cluster"
-  value       = aws_eks_cluster.demo.name
-}
-
-output "eks_cluster_endpoint" {
-  description = "The endpoint of cluster"
-  value       = aws_eks_cluster.demo.endpoint
-}
